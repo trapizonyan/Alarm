213126b3e4dbd8357d512ed0aa2761a10a46d16a
Трапизонян Рафик <70804434+trapizonyan@users.noreply.github.com>
2021-10-05 02:37:26 +0300

    Тесты для будильника
    + метод тестирования времени

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import unittest
 from Alarm import Alarm
 from Interface import Interface
+from Clock import Clock
 
 
 class TestAlarm(unittest.TestCase):
 
     def setUp(self):
         self.ob_alarm = Alarm()
+        self.ob_cl = Clock()
 
     def test_music_load(self):
         self.assertEqual(self.ob_alarm.music_load('one'), True)
@@ -16,6 +18,9 @@ class TestAlarm(unittest.TestCase):
 
     def test_music_list(self):
         self.assertEqual(self.ob_alarm.music_list['two'], 'media/2.mp3')
+
+    def test_format_clock(self):
+        self.assertEqual(self.ob_cl.format_clock(), '02:34:57')
 
 
 if __name__ == '__main__':
